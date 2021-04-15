35b88bcbf8ce6f3cd19cdcf1a4d95a01d3aad759
Lais Teixeira <laistei@gmail.com>
2021-04-14 22:18:14 -0300

    atualização do banco de dados

--- ScritpCriacao.R
+++ ScritpCriacao.R
@@ -1,4 +1,7 @@
 #usethis::crearte_project("C:\\Projeto\\lab_ciencia_dados")
 
 #Criar a estrutura GIT para esse projeto
-usethis::use_git()
+#usethis::use_git()
+##usethis::use_github()
+##usethis::create_github_token()
+##gitcreds::gitcreds_set() #nao precisa de URL'
